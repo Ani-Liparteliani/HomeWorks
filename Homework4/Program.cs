0cd943146f7b69e43bf7e0effa96ad6bc65bd24b
ann liparteliani <annliparteliani1@outlook.com>
2025-09-01 13:31:24 +0400

    Homework 4

---
+++ Homework4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+                Console.WriteLine("Ani Liparteliani");
+
+                Console.WriteLine("Enter input as text: ");
+                string input = Console.ReadLine();
+                Console.WriteLine(input);
+            
+        }
+    }
+}
